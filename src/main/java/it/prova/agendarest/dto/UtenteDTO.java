3803d4e97e87690545c36487e29d47ea55b1f0ce
Rossenberg Villanueva Ramboanga <rossenbergvillanueva.ramboanga@gmail.com>
2022-11-28 15:44:57 +0100

    Implemented Utente DTO

---
+++ src/main/java/it/prova/agendarest/dto/UtenteDTO.java
@@ -0,0 +1,154 @@
+package it.prova.agendarest.dto;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import it.prova.agendarest.model.Ruolo;
+import it.prova.agendarest.model.StatoUtente;
+import it.prova.agendarest.model.Utente;
+
+public class UtenteDTO {
+
+	private Long id;
+
+	@NotBlank(message = "{username.notblank}")
+	@Size(min = 3, max = 15, message = "Il valore inserito '${validatedValue}' deve essere lungo tra {min} e {max} caratteri")
+	private String username;
+
+	@NotBlank(message = "{password.notblank}")
+	@Size(min = 8, max = 15, message = "Il valore inserito deve essere lungo tra {min} e {max} caratteri")
+	private String password;
+
+	private String confermaPassword;
+
+	@NotBlank(message = "{nome.notblank}")
+	private String nome;
+
+	@NotBlank(message = "{cognome.notblank}")
+	private String cognome;
+
+	@NotBlank(message = "{email.notblank}")
+	private String email;
+
+	private Date dateCreated;
+
+	private StatoUtente stato;
+
+	private Long[] ruoliIds;
+
+	public UtenteDTO() {
+	}
+
+	public UtenteDTO(Long id, String username, String nome, String cognome, StatoUtente stato) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.stato = stato;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public StatoUtente getStato() {
+		return stato;
+	}
+
+	public void setStato(StatoUtente stato) {
+		this.stato = stato;
+	}
+
+	public String getConfermaPassword() {
+		return confermaPassword;
+	}
+
+	public void setConfermaPassword(String confermaPassword) {
+		this.confermaPassword = confermaPassword;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long[] getRuoliIds() {
+		return ruoliIds;
+	}
+
+	public void setRuoliIds(Long[] ruoliIds) {
+		this.ruoliIds = ruoliIds;
+	}
+
+	public Utente buildUtenteModel(boolean includeIdRoles) {
+		Utente result = new Utente(this.id, this.username, this.password, this.nome, this.cognome, this.email,
+				this.dateCreated, this.stato);
+		if (includeIdRoles && ruoliIds != null)
+			result.setRuoli(Arrays.asList(ruoliIds).stream().map(id -> new Ruolo(id)).collect(Collectors.toSet()));
+
+		return result;
+	}
+
+	public static UtenteDTO buildUtenteDTOFromModel(Utente utenteModel) {
+		UtenteDTO result = new UtenteDTO(utenteModel.getId(), utenteModel.getUsername(), utenteModel.getNome(),
+				utenteModel.getCognome(), utenteModel.getStato());
+
+		if (!utenteModel.getRuoli().isEmpty())
+			result.ruoliIds = utenteModel.getRuoli().stream().map(r -> r.getId()).collect(Collectors.toList())
+					.toArray(new Long[] {});
+
+		return result;
+	}
+}
